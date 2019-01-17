39db29252549f11f7f8bbde1c7c5ac9cab8852a6
Sander Mertens <sander.mertens8@gmail.com>
2019-01-16 19:30:30 -0800

    Initial commit

---
+++ include/math.h
@@ -0,0 +1,18 @@
+#ifndef REFLECS_MATH_H
+#define REFLECS_MATH_H
+
+/* This generated file contains includes for project dependencies */
+#include "bake_config.h"
+
+#include "vector.h"
+#include "matrix.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
