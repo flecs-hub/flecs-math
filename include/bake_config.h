bca3cc36bccda73159b1a3146dfeb0f0a29342c1
Sander Mertens <sander.mertens8@gmail.com>
2019-03-30 10:57:07 -0700

    Update generated bake header

--- include/bake_config.h
+++ include/bake_config.h
@@ -17,6 +17,10 @@
 #ifndef FLECS_MATH_BAKE_CONFIG_H
 #define FLECS_MATH_BAKE_CONFIG_H
 
+/* Generated includes are specific to the bake environment. If a project is not
+ * built with bake, it will have to provide alternative methods for including
+ * its dependencies. */
+#ifdef __BAKE__
 /* Headers of public dependencies */
 /* No dependencies */
 
@@ -24,16 +28,21 @@
 #ifdef FLECS_MATH_IMPL
 /* No dependencies */
 #endif
+#endif
 
 /* Convenience macro for exporting symbols */
-#if FLECS_MATH_IMPL && defined _MSC_VER
-#define FLECS_MATH_EXPORT __declspec(dllexport)
-#elif FLECS_MATH_IMPL
-#define FLECS_MATH_EXPORT __attribute__((__visibility__("default")))
-#elif defined _MSC_VER
-#define FLECS_MATH_EXPORT __declspec(dllimport)
+#ifndef FLECS_MATH_STATIC
+  #if FLECS_MATH_IMPL && defined _MSC_VER
+    #define FLECS_MATH_EXPORT __declspec(dllexport)
+  #elif FLECS_MATH_IMPL
+    #define FLECS_MATH_EXPORT __attribute__((__visibility__("default")))
+  #elif defined _MSC_VER
+    #define FLECS_MATH_EXPORT __declspec(dllimport)
+  #else
+    #define FLECS_MATH_EXPORT
+  #endif
 #else
-#define FLECS_MATH_EXPORT
+  #define FLECS_MATH_EXPORT
 #endif
 
 #endif
